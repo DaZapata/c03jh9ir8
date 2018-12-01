1f910e294723b1d3bfe6f0aa21ac77edf3890c4b
David <davidzapata56@gmail.com>
2018-12-01 09:34:24 -0500

    actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,35 @@
 class ActorsController < ApplicationController
+
+  def index
+  		@actores=Actor.all
+  	end
+
+  	def new
+
+  	end
+
+  	def create
+  		if params['alive']=="on"
+  			alive=true
+
+  		else
+  			alive=false
+  		end
+
+  		Actor.create(
+  			name: params['actor_name'],
+  			image_url:params['image_url'],
+  			bio: params['bio'],
+  			birth_date: params['dob'],
+  			birth_place:params['pob'],
+  			alive: alive,
+  			death_date: params['dod'],
+  			death_place: params['pod',]
+  			)
+
+  		redirect_to actors_path
+
+  	end
+
+
 end
